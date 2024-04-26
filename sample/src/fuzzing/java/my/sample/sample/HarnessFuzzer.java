7060bc8d64a6eb901017e10ca19ab584f2730333
tvistoman <63655851+tvistoman@users.noreply.github.com>
2024-04-26 16:14:33 +0300

    Update HarnessFuzzer.java

--- sample/src/fuzzing/java/my/sample/sample/HarnessFuzzer.java
+++ sample/src/fuzzing/java/my/sample/sample/HarnessFuzzer.java
@@ -1,1 +1,30 @@
+package my.sample.sample;
 
+// import com.fasterxml.jackson.core.json.*;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.io.File;
+import java.io.IOException;
+
+import my.sample.sample.controller.BookController;
+import my.sample.sample.dto.Book;
+import my.sample.sample.service.BookServiceImpl; 
+
+import com.code_intelligence.jazzer.api.FuzzedDataProvider;
+import com.code_intelligence.jazzer.api.FuzzerSecurityIssueHigh;
+
+public class HarnessFuzzer {
+    public static void fuzzerTestOneInput(FuzzedDataProvider fuzzedDataProvider) {
+        ObjectMapper objectMapper = new ObjectMapper();
+        BookController obj = new BookController();
+        Book book;
+        String content = fuzzedDataProvider.consumeRemainingAsAsciiString();
+        try {
+            book = objectMapper.readValue(content, Book.class); 
+        } catch (IOException ioe) {
+            System.out.println(ioe);
+            return;
+        }
+        obj.addBook(book);
+    }
+}
